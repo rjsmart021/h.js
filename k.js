7d9a02cbf9f3dead5a320060a845349d50d95208
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-15 16:34:18 -0600

    Update k.js

--- k.js
+++ k.js
@@ -1,15 +1,42 @@
 //Module 11 Lesson 2: Assignments | React State and Props
 //Task 1: Code Correction
 //The current implementation of the user profile component has a bug in state management. Analyze and correct the code to ensure the user's name is displayed correctly.
+import React, { Component } from 'react';
+
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      age: null,
+      email: ''
+    };
+  }
+
+  componentDidMount() {
+    // Fetch user data or perform any setup here
+  }
+
+  render() {
+    const { name, age, email } = this.state;
+    return (
+      <div>
+        <h1>User Profile</h1>
+        <p>Name: {name: 'Alex'}</p>
+        <p>Age: {age}</p>
+        <p>Email: {email}</p>
+      </div>
+    );
+  }
+}
 class UserProfile extends Component {
     constructor(props) {
         super(props);
-        this.state = { name: 'Alex' };
+        this.state = { name: 'Kb' };
     }
 
-    // Incorrect function
-    changeName() {
-        this.state.name = 'Charlie';
+    changeName = () => {
+        this.setState({ name: 'Johna' });
     }
 
     render() {
@@ -22,3 +49,5 @@ class UserProfile extends Component {
         );
     }
 }
+
+export default UserProfile;
