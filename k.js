666c4398b695c94bf1ede46f169b225d0c8bcb67
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-15 14:28:23 -0600

    Update k.js

--- k.js
+++ k.js
@@ -1,3 +1,24 @@
 //Module 11 Lesson 2: Assignments | React State and Props
 //Task 1: Code Correction
 //The current implementation of the user profile component has a bug in state management. Analyze and correct the code to ensure the user's name is displayed correctly.
+class UserProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: 'Alex' };
+    }
+
+    // Incorrect function
+    changeName() {
+        this.state.name = 'Charlie';
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>User Profile</h1>
+                <p>Name: {this.state.name}</p>
+                <button onClick={this.changeName}>Change Name</button>
+            </div>
+        );
+    }
+}
