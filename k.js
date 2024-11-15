8781d40ef6974180356d5d99e6419c2b6923f5a3
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-15 14:27:31 -0600

    Update and rename k to k.js

---
+++ k.js
@@ -0,0 +1,3 @@
+//Module 11 Lesson 2: Assignments | React State and Props
+//Task 1: Code Correction
+//The current implementation of the user profile component has a bug in state management. Analyze and correct the code to ensure the user's name is displayed correctly.
